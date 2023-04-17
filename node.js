79605155b31dff37e4c0efc00fda14c2f05cba66
vamshi <alugojuvamshi@gmail.com>
2023-04-17 18:31:54 +0530

    first push

--- node.js
+++ node.js
@@ -1,1 +1,14 @@
 console.log("hello world")
+console.log("hello world")
+ 
+const { fstat } = require("fs");
+const http = require("http");
+
+const server = http.createServer((req,res)=>{
+   // console.log("server running");
+   console.log(req)
+})
+
+server.listen(4000)
+
+
