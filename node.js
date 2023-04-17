74cd4e4f10a2a51279a6863c1ed194954b1307e6
vamshi <alugojuvamshi@gmail.com>
2023-04-17 18:36:47 +0530

    first push

--- node.js
+++ node.js
@@ -5,8 +5,25 @@ const { fstat } = require("fs");
 const http = require("http");
 
 const server = http.createServer((req,res)=>{
-   // console.log("server running");
-   console.log(req)
+    
+    const url = req.url;
+    
+    switch (url) {
+        case "/home":
+            res.write("welcome to home")
+            break;
+        case "/node":
+            res.write("welcome to my nodejs project")
+            break;
+        case "/about":
+            res.write("welcome to about us page")
+            break;
+    
+        default:
+            res.write("enter a valid url")
+            break;
+    }
+    res.end();
 })
 
 server.listen(4000)
